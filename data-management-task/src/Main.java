2e12315d5ceb05fecefa931303f279abef13a82f
Stoyan Stoyanov <SStoyanov22@gmail.com>
2022-11-08 10:44:15 +0200

    Adding tsv reader

--- data-management-task/src/Main.java
+++ data-management-task/src/Main.java
@@ -1,5 +1,27 @@
+import javax.xml.crypto.Data;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var customers_data = tsvReader(new File("sample_data\\customers.tsv"));
+        System.out.println(customers_data);
+    }
+
+   public static ArrayList<String[]> tsvReader(File test2) {
+        ArrayList<String[]> Data = new ArrayList<>(); //initializing a new ArrayList out of String[]'s
+        try (BufferedReader TSVReader = new BufferedReader(new FileReader(test2))) {
+            String line = null;
+            while ((line = TSVReader.readLine()) != null) {
+                String[] lineItems = line.split("\t"); //splitting the line and adding its items in String[]
+                Data.add(lineItems); //adding the splitted line array to the ArrayList
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+        return Data;
     }
 }
