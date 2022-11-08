f6156589339c04514fc9829655e078e6c489a23e
SStoyanov22 <SStoyanov22@gmail.com>
2022-11-08 21:34:48 +0200

    Added importing of data

---
+++ data-management-task/src/ProductDTO.java
@@ -0,0 +1,9 @@
+public class ProductDTO {
+    public final String product_id;
+    public final String name;
+
+    public ProductDTO(String product_id, String name) {
+        this.product_id = product_id;
+        this.name = name;
+    }
+}
